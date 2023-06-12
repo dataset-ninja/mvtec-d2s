9fa2145cddc4ca14a2777ad0a29b0da21ec886fc
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-06-12 14:48:25 +0300

    Updated for new template.

--- src/convert.py
+++ src/convert.py
@@ -1,20 +1,20 @@
-import supervisely as sly
 import os
-from dotenv import load_dotenv
+
 import supervisely as sly
-
-if sly.is_development():
-    load_dotenv(os.path.expanduser("~/ninja.env"))
-    load_dotenv("local.env")
-
 from dataset_tools.convert.mvtecd2s.main import to_supervisely
+from dotenv import load_dotenv
 
-api = sly.Api.from_env()
-workspace_id = sly.env.workspace_id()
+# if sly.is_development():
+#   load_dotenv(os.path.expanduser("~/ninja.env"))
+#    load_dotenv("local.env")
 
-project_id = to_supervisely(api, workspace_id)
 
-print("Project id is", project_id)
+# api = sly.Api.from_env()
+# workspace_id = sly.env.workspace_id()
+
+# project_id = to_supervisely(api, workspace_id)
+
+# print("Project id is", project_id)
 
 
 def convert_and_upload_supervisely_project(
