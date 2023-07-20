aa5ab553ee1240d2b7776a517f764e00f56182fe
Ars Vladimirov <grokhael@gmail.com>
2023-07-20 21:13:18 +0600

    upd settings

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -12,18 +20,23 @@ PROJECT_NAME_FULL: Optional[str] = "MVTEC D2S Densely Segmented Supermarket Data
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.BY_NC_SA_4_0()
-INDUSTRIES: List[Industry] = [Industry.Industrial()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.Industrial()]
+CATEGORY: Category = Category.Benchmark()
+
 CV_TASKS: List[CVTask] = [
     CVTask.SemanticSegmentation(),
     CVTask.InstanceSegmentation(),
     CVTask.ObjectDetection(),
 ]
 ANNOTATION_TYPES: List[AnnotationType] = [
-    AnnotationType.SemanticSegmentation(),
+    AnnotationType.InstanceSegmentation(),
     AnnotationType.ObjectDetection(),
 ]
 
-RELEASE_YEAR: int = 2018
+RELEASE_DATE: Optional[str] = "2018-04-23"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = "https://www.mvtec.com/company/research/datasets/mvtec-d2s"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -42,10 +55,29 @@ DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
+PAPER: Optional[
+    str
+] = "https://www.mvtec.com/fileadmin/Redaktion/mvtec.com/company/research/datasets/mvtec_d2s.pdf"
 CITATION_URL: Optional[str] = None
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+AUTHORS: Optional[List[str]] = [
+    "Patrick Follmann",
+    "Tobias Böttger",
+    "Philipp Härtinger",
+    "Rebecca König",
+    "Markus Ulrich",
+]
+
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "MVTec Software GmbH, Germany",
+    "Technical University of Munich",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://www.mvtec.com/company/research",
+    "https://www.tum.de/en/",
+]
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
+
 TAGS: List[str] = None
 
 ##################################
@@ -60,10 +92,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -75,13 +112,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
     settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
