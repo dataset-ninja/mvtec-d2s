1af93f1515bb8e7f13f3922561b7de9ecae24fbb
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-07 12:01:07 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -40,7 +40,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.mvtec.com/company/research/datasets/mvtec-d2s"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 347893
+PREVIEW_IMAGE_ID: int = 1811026
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/mvtec-d2s"
